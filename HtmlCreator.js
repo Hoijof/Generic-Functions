3ec1a0d169c790203b0c21d8363f3d2d8bfdd0c9
Humberto Gómez López <hoijof@iyv-corp.com>
2014-09-02 16:42:57 +0200

    Edited core of HtmlCreatior
    - Edited names of HtmlCreator

--- HtmlCreator.js
+++ HtmlCreator.js
@@ -1,22 +1,20 @@
-function HtmlCreation () {
-    
-        }
+function HtmlCreator () {}
 
-HtmlCreation.createElem = function (elem, id, className, content, attributes) {
+HtmlCreator.createElem = function (elem, id, className, content, attributes) {
     return (typeof attributes != 'undefined') ? '<' + elem +' id="' + id + '" class="' + className + '" ' + attributes
             + '>' + content + '</' + elem + '>' : '<' + elem +' id="' + id + '" class="' + className + '">' + content + '</' + elem + '>';
 };
 
-HtmlCreation.createMonoElem = function (elem, id, className, attributes) {
+HtmlCreator.createMonoElem = function (elem, id, className, attributes) {
     return (typeof attributes != 'undefined') ? '<' + elem +' id="' + id + '" class="' + className + '" ' + attributes
             + '>' : '<' + elem +' id="' + id + '" class="' + className + '">';
 };
 
-HtmlCreation.createHorizontalLine = function (className) {
+HtmlCreator.createHorizontalLine = function (className) {
     return (typeof className != 'undefined') ? '<hr class="' + className + '"/>' : '<hr/>';
 };
 
-HtmlCreation.createListFromObject = function (object, listId, listClass, translation) {
+HtmlCreator.createListFromObject = function (object, listId, listClass, translation) {
     var html = "<select id='"+listId+"' class='"+listClass+"'>";
 
         $.each(object, function(key, value) {
